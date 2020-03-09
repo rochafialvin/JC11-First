71fa788a887f221ead1336f3a79ddb262b17723d
rochafialvin <rochafi.dev@gmail.com>
2020-03-09 11:43:02 +0700

    res.data

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 class SearchBar extends React.Component {
 
@@ -6,6 +7,23 @@ class SearchBar extends React.Component {
     onSubmitForm = (event) => {
         // Agar halaman tidak me refresh / reload
         event.preventDefault()
+
+        // Akses Unsplash API
+        axios.get(
+            'https://api.unsplash.com/search/photos',
+            {
+                headers: {
+                    Authorization: 'Client-ID A-c3T4tZE8CTC4eJ0liT89vki1JMBaUmHO2zJ9RJ-OY'
+                },
+
+                params: {
+                    query: 'cat',
+                    per_page: 15
+                }
+            }
+        ).then((res) => {console.log(res.data)})
+
+
     }
 
     render() {
